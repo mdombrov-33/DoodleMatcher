6e342806b0ed65c48a7ffd6036e250b04e7c4d68
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-24 17:36:24 +0300

    fix(docker): fix docker issues

--- backend/routes/health.py
+++ backend/routes/health.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status
 
 router = APIRouter()
 
-router.get("/health", status_code=status.HTTP_200_OK, summary="Health check")
+
+@router.get("/health", status_code=status.HTTP_200_OK, summary="Health check")
 async def health_check():
     return {"status": "healthy"}
